14fa78ca311bb1767a4d4074c2c121089569dc90
Andy Gallagher <andy.gallagher@theguardian.com>
2014-01-07 22:01:21 +0000

    Mostly working version of cdsresponder, which downloads and names files correctly

--- cdsresponder.rb
+++ cdsresponder.rb
@@ -27,26 +27,59 @@ def initialize(arn,routename,arg)
 	}
 end
 
-#Download the route name given from the DynamoDB table and return the local filename
-def DownloadRoute
+def GetUniqueFilename(path)
+filebase=@routename.gsub(/[^\w\d]/,"_")
+
+filename=path+'/'+filebase+".xml"
+
+n=0;
+while(Pathname(filename).exist?) do
+	n=n+1
+	filename=path+'/'+filebase+"-"+n.to_s()+".xml"
+end
+filename
+end
+
+def GetRouteContent
+ddb=AWS::DynamoDB.new(:region=>'eu-west-1')
+table=ddb.tables['workflowmaster-cds-routes']
+table.hash_key=[ :routename,:string ]
+item=table.items[@routename]
+item.attributes['content']
 
 end
 
-#Output the message as a trigger file
-def OutputTriggerFile(contents)
+#Download the route name given from the DynamoDB table and return the local filename
+def DownloadRoute
+filename=GetUniqueFilename('/etc/cds_backend/routes')
 
-"testfile"
+puts "Got filename #{filename} for route file\n"
+
+File.open(filename, 'w'){ |f|
+	f.write(GetRouteContent())
+}
+filename
+end
+
+#Output the message as a trigger file
+def OutputTriggerFile(contents,id)
+
+File.open(id+".xml", 'w'){ |f|
+	f.write(contents)
+}
+id+".xml"
 end
 
 def ThreadFunc
 
 while @isexecuting do
 	@q.receive_message { |msg|
-		puts "Received message:\n";
+		#puts "Received message:\n";
 		#puts "\t#{msg.body}\n";
-		triggerfile=OutputTriggerFile(msg.body)
+		triggerfile=OutputTriggerFile(msg.body,msg.id)
 		cmd=@commandline + triggerfile + " --logging-id=#{msg.id}"
-		puts cmd
+		system(cmd)
+		File.delete(triggerfile)
 	}
 end
 
@@ -54,6 +87,7 @@ end
 
 def join
 @threadref.join
+File.delete(@routefile)
 end
 
 end
