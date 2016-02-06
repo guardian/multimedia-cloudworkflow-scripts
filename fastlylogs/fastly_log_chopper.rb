3a570bbbd65ed2f6bceb3906c84059c0c37b210c
Andy Gallagher <andy.gallagher@theguardian.com>
2016-02-06 11:23:38 +0000

    ensuring that geoip data is always available. sundry improvements to fastly_log_chopper

--- fastlylogs/fastly_log_chopper.rb
+++ fastlylogs/fastly_log_chopper.rb
@@ -9,13 +9,12 @@ require 'trollop'
 require 'aws-sdk-resources'
 require 'aws-sdk-core'
 require 'json'
+require 'base64'
 
 INDEXNAME='fastlylogs'
 TYPENAME="log"
+LOGFILE = "/var/log/fastly_log_chopper.log"
 
-$logger=Logger.new("/var/log/fastly_log_chopper.log")
-#$logger=Logger.new(STDOUT)
-$logger.level=Logger::DEBUG
 
 class ElasticIndexer
   def initialize(client: nil,autocommit: 0)
@@ -51,12 +50,12 @@ class ElasticIndexer
     return h
   end #def flatten.hash
   
-  def add_record(rec)
+  def add_record(rec, rec_id: nil)
     #$logger.info("adding record")
     if rec.is_a?(Hash)
-      @records << self.flatten_hash(rec)
+      @records << [self.flatten_hash(rec), rec_id]
     else
-      @records << rec
+      @records << [rec, rec_id]
     end
     if @records.length > @autocommit_threshold
       self.commit
@@ -66,12 +65,16 @@ class ElasticIndexer
   def commit
     actions = []
     $logger.info("Committing to index #{INDEXNAME}...")
-    @records.each do |rec|
-      actions << { index: {
-        _index: INDEXNAME,
-        _type: TYPENAME,
-        data: rec
-      }}
+    @records.each do |ent|
+        rec = ent[0]
+        rec_id = ent[1]
+        data = {
+            _index: INDEXNAME,
+            _type: TYPENAME,
+            data: rec
+        }
+        data['_id'] = rec_id if(rec_id != nil)
+      actions << { index: data}
     end
     @client.bulk(body: actions)
     @records = []
@@ -105,6 +108,32 @@ def podcast_details(path)
 
 end
 
+def video_details(target_url)
+    base = File.basename(target_url)
+    #remove any url args
+    base.sub!(/[?;].*$/,'')
+    
+    rtn = {}
+    
+    parts = /^(.*)\.([^\.]+)/.match(base)
+    if(parts)
+        #parts[1] is filename, parts[2] is extension
+        filename_tokens = parts[1].split(/_/)
+        if(parts[2] == "m3u8") #these have extra tokens
+            rtn['encoding_type']='HLS'
+            if(filename_tokens[-1].match(/\d+[mMkK]{0,1}/)) #this has a bitrate specifier so is a submanifest
+               rtn['manifest_type'] = 'submanifest'
+            else
+               rtn['manifest_type'] = 'master'
+            end
+        else
+            rtn['encoding_type'] = filename_tokens[-1]
+        end
+     end #if(parts)
+    return nil if(rtn.count==0)
+    return rtn
+end #def video_details
+
 def parse_string(str,extra_data: {}, indexer: nil)
   #grok = Grok.new
   #grok.add_patterns_from_file('patterns/base')
@@ -135,7 +164,7 @@ def parse_string(str,extra_data: {}, indexer: nil)
   end
   
   matcher = Regexp.new('(?<datestamp>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z) (?<pop>[\w\d\-]+) (?<destination>[\w\d]+)\[(?<pid>\d+)\]: (?<client>\d+\.\d+\.\d+\.\d+) \".*\" \".*\" .* (?<verb>\w+) (?<target>[^\s]+) (?<response>\d+)$')
-  str.split(/\n/).each {|line|
+  str.encode('utf-8').split(/\n/).each {|line|
     #puts line
     line.chomp!
     match=matcher.match(line)
@@ -162,6 +191,13 @@ def parse_string(str,extra_data: {}, indexer: nil)
       }
     end
     
+    target_details=video_details(rtn['target'])
+    if target_details
+        target_details.each {|k,v|
+            rtn[k]=v
+        }
+    end
+    
     if g
       countrydata=g.country(rtn['client'])
       rtn['country_code']=countrydata[:country_code]
@@ -173,24 +209,27 @@ def parse_string(str,extra_data: {}, indexer: nil)
     
     if c
       citydata=c.city(rtn['client'])
-      rtn['city_name']=citydata[:city_name]
-      rtn['postal_code']=citydata[:postal_code]
-      #rtn['latitude']=citydata[:latitude]
-      #rtn['longitude']=citydata[:longitude]
-      rtn['location'] = {
-        'lat' => citydata[:latitude],
-        'lon' => citydata[:longitude]
-      }
-      rtn['dma_code']=citydata[:dma_code]
-      rtn['area_code']=citydata[:area_code]
-      rtn['timezone']=citydata[:timezone]
-      rtn['region_name']=citydata[:real_region_name]
-      #ap citydata
+      if citydata
+          rtn['city_name']=citydata[:city_name]
+          rtn['postal_code']=citydata[:postal_code]
+          #rtn['latitude']=citydata[:latitude]
+          #rtn['longitude']=citydata[:longitude]
+          rtn['location'] = {
+            'lat' => citydata[:latitude],
+            'lon' => citydata[:longitude]
+          }
+          rtn['dma_code']=citydata[:dma_code]
+          rtn['area_code']=citydata[:area_code]
+          rtn['timezone']=citydata[:timezone]
+          rtn['region_name']=citydata[:real_region_name]
+          #ap citydata
+      end
     end
     
     rtn.merge!(extra_data)
     #raise StandardError, "Testing"
-    indexer.add_record(rtn)
+    doc_id = Base64.encode64(rtn['line'])
+    indexer.add_record(rtn, rec_id: doc_id)
   }
   indexer.commit
 end
@@ -225,48 +264,84 @@ $opts = Trollop::options do
   opt :elasticsearch, "Location of elasticsearch cluster to communicate with. Specify multiple hosts separated by commas.", :type=>:string, :default=>"localhost"
   opt :queueurl, "URL of the Amazon SQS queue to listen to", :type=>:string
   opt :region, "AWS region to operate in", :type=>:string, :default=>"eu-west-1"
+  opt :testfile, "Test run on the provided file", :type=>:string
+  opt :reindex_bucket, "Re-index from the specified bucket", :type=>:string
 end
 
-if $opts[:queueurl]==nil
-  $logger.error("You need to specify a queue to listen to using --queueurl")
-  exit(1)
+if($opts.testfile)
+    $logger = Logger.new(STDOUT)
+    $logger.level = Logger.DEBUG
+    $logger.info("Starting test on #{$opts.testfile}")
+    File.open($opts.testfile) do |f|
+        parse_string(f.read,extra_data: {'domain' => '(test)'}, indexer: ElasticIndexer.new(client: ets, autocommit: 500))
+    end #File.open
+    exit(0)
 end
+
+$logger=Logger.new(LOGFILE)
+#$logger=Logger.new(STDOUT)
+$logger.level=Logger::DEBUG
 
 ets = Elasticsearch::Client.new(hosts: $opts.elasticsearch.split(/,\s*/),log: true)
 ets.cluster.health
 
 if not ets.indices.exists?(index: INDEXNAME)
-  ets.indices.create(index: INDEXNAME,body: {
-                      settings: {
-                        analysis: {
-                          analyzer: {
-                            path: {
-                              tokenizer: "path_hierarchy",
-                              type: "custom",
-                            }
-                          }
-                        }
-                      },
-                      mappings: {
-                        log: {
-                          properties: {
-                            pop: {type: "string", index: "not_analyzed"},
-                            city_name: {type: "string", index: "not_analyzed"},
-                            continent_code: {type: "string", index: "not_analyzed"},
-                            country_name: {type: "string", index: "not_analyzed"},
-                            client: {type: "ip"},
-                            location: {type: "geo_point"},
-                            filename: {type: "string", index: "not_analyzed"},
-                            postal_code: {type: "string", index: "not_analyzed"},
-                            region_name: {type: "string", index: "not_analyzed"},
-                            section: {type: "string", index: "not_analyzed"},
-                            series: {type: "string", index: "not_analyzed"},
-                            target: {type: "string", analyzer: "path"},
-                     file_extension: {type: "string", index: "not_analyzed"}
-                          }
-                        }
-                      }
-  })
+    ets.indices.create(index: INDEXNAME,body: {
+                       settings: {
+                       analysis: {
+                       analyzer: {
+                       path: {
+                       tokenizer: "path_hierarchy",
+                       type: "custom",
+                       }
+                       }
+                       }
+                       },
+                       mappings: {
+                       log: {
+                       properties: {
+                       pop: {type: "string", index: "not_analyzed"},
+                       city_name: {type: "string", index: "not_analyzed"},
+                       continent_code: {type: "string", index: "not_analyzed"},
+                       country_name: {type: "string", index: "not_analyzed"},
+                       client: {type: "ip"},
+                       location: {type: "geo_point"},
+                       filename: {type: "string", index: "not_analyzed"},
+                       postal_code: {type: "string", index: "not_analyzed"},
+                       region_name: {type: "string", index: "not_analyzed"},
+                       section: {type: "string", index: "not_analyzed"},
+                       series: {type: "string", index: "not_analyzed"},
+                       target: {type: "string", analyzer: "path"},
+                       file_extension: {type: "string", index: "not_analyzed"}
+                       }
+                       }
+                       }
+                       })
+end
+
+if($opts.reindex_bucket)
+    $logger.info("Attempting re-index from #{$opts.reindex_bucket}")
+    s3 = Aws::S3::Client.new(region: $opts.region)
+    b = Aws::S3::Bucket.new($opts.reindex_bucket, client: s3)
+    
+    b.objects.each do |obj|
+        content = download_from_s3(bucket: $opts.reindex_bucket,key: obj.key)
+        domain = obj.key.split(/\//)[0]
+        if domain == "" #leading / confuses things somewhat
+            domain = obj.key.split(/\//)[1]
+        end
+        
+        #raise StandardError, "Testing"
+        $logger.info("Parsing...")
+        parse_string(content,extra_data: {'domain' => domain}, indexer: ElasticIndexer.new(client: ets,autocommit: 500))
+        $logger.info("Done.")
+    end #b.objects.each
+    exit(0)
+end
+
+if $opts[:queueurl]==nil
+  $logger.error("You need to specify a queue to listen to using --queueurl")
+  exit(1)
 end
 
 c=Aws::SQS::Client.new(region: $opts.region)
