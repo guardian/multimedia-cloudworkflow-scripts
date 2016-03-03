5fbeb3114ba976c781cdc3352e043eceac44d661
Andy Gallagher <andy.gallagher@theguardian.com>
2016-03-03 12:41:18 +0000

    adding logpurge script and endpoint updates

---
+++ fastlylogs/logpurge.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+require 'logger'
+require 'elasticsearch'
+require 'trollop'
+require 'awesome_print'
+
+INDEXNAME="fastlylogs"
+
+#START MAIN
+opts = Trollop::options do
+  opt :elasticsearch, "Elasticsearch hosts", :type=>:string
+  opt :repository, "Elasticsearch snapshot repository", :type=>:string, :default=>"s3_snapshot_repo"
+end
+
+logger = Logger.new(STDOUT)
+
+logger.info("Connecting to Elasticsearch on #{opts.elasticsearch}")
+es = Elasticsearch::Client.new(:hosts=>opts.elasticsearch)
+
+
+timestr = DateTime.now.strftime("%Y%m%d_%H%M%S")
+snap_name = "#{INDEXNAME}_automated_#{timestr}"
+logger.info("Starting backup to #{opts.repository} : #{snap_name}")
+
+es.snapshot.create(repository: opts.repository, snapshot: snap_name, wait_for_completion: true)
+logger.info("Completed")
+
+logger.info("Starting autopurge...")
+resultset = es.search(index: INDEXNAME, search_type: "scan", scroll: "5m", body: {
+  'query'=> {
+    'filtered'=> {
+      'filter'=> {
+        'range'=> {
+          'datestamp'=> {
+            "lte"=>"now-15d"
+          }
+        }
+      }
+    }
+  }
+})
+
+
+ap resultset['_scroll_id']
+
+while resultset = es.scroll(scroll_id: resultset['_scroll_id'], scroll: "5m") and not resultset['hits']['hits'].empty? do
+  bulklist = []
+  resultset['hits']['hits'].each {|entry|
+    bulklist << { delete: {_id: entry['_id']}}
+  }
+  ap bulklist
+end
+logger.info("Done")
